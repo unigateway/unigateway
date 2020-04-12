9182eef471f9d16ef478a631b075bd5097f0d0df
Mariusz <mariusz@aetas.pl>
2020-04-12 17:26:38 +0200

    Add timeout to be sure test will finish in sensible time E.g. in case of MQTT connection problems

--- src/test/groovy/com/mqgateway/ApplicationStartsTest.groovy
+++ src/test/groovy/com/mqgateway/ApplicationStartsTest.groovy
@@ -2,10 +2,12 @@ package com.mqgateway
 
 
 import spock.lang.Specification
+import spock.lang.Timeout
 
 /**
  * These tests can only be started with ENV SimulatedPlatform=com.pi4j.gpio.extension.mcp.MCP23017GpioProvider
  */
+@Timeout(60)
 class ApplicationStartsTest extends Specification {
 
 	def "should start application with simulated GPIOs"() {
