5cd35ab54c4880c888d15ea76f7de08d5e232ea0
Mariusz <mariusz@aetas.pl>
2020-05-03 12:25:04 +0200

    Do not expect system to respond within 100ms (running tests on GitHub actions is sloooow)

--- src/test/groovy/com/mqgateway/core/onewire/OneWireBusTest.groovy
+++ src/test/groovy/com/mqgateway/core/onewire/OneWireBusTest.groovy
@@ -2,15 +2,16 @@ package com.mqgateway.core.onewire
 
 import java.util.concurrent.TimeUnit
 import spock.lang.Specification
+import spock.lang.Timeout
 import spock.util.concurrent.BlockingVariable
 
+@Timeout(10)
 class OneWireBusTest extends Specification {
 
 	def "should check value for all registered devices on given interval"() {
 		given:
 		File tempDir = File.createTempDir()
 		BlockingVariable<String> blockingVariable = new BlockingVariable<>(10, TimeUnit.SECONDS)
-		long testStartTimestamp = System.currentTimeMillis()
 		def oneWireBus = new OneWireBus(tempDir.absolutePath, 5)
 		def fakeDevice = new OneWireBusDeviceStub("123456")
 		fakeDevice.addDeviceConnectionListener {event -> blockingVariable.set("this is new value") }
@@ -21,7 +22,6 @@ class OneWireBusTest extends Specification {
 
 		then:
 		blockingVariable.get() == "this is new value"
-		testStartTimestamp + 100 > System.currentTimeMillis()
 	}
 }
 
