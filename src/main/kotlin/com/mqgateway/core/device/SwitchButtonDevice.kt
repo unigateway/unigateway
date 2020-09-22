c0a8b97d958cf98d9a7bfdfbde7dfa5a31f7403c
Mariusz <mariusz@aetas.pl>
2020-09-23 00:03:02 +0200

    Corrections for LONG_PRESS in SwitchButtonDevice

--- src/main/kotlin/com/mqgateway/core/device/SwitchButtonDevice.kt
+++ src/main/kotlin/com/mqgateway/core/device/SwitchButtonDevice.kt
@@ -4,7 +4,9 @@ import com.mqgateway.core.gatewayconfig.DevicePropertyType.STATE
 import com.mqgateway.core.gatewayconfig.DeviceType
 import com.pi4j.io.gpio.GpioPinDigitalInput
 import mu.KotlinLogging
-import kotlin.concurrent.thread
+import java.time.Instant
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 private val LOGGER = KotlinLogging.logger {}
 
@@ -15,7 +17,9 @@ class SwitchButtonDevice(
   private val longPressTimeMs: Long = CONFIG_LONG_PRESS_TIME_MS_DEFAULT
 ) : DigitalInputDevice(id, DeviceType.SWITCH_BUTTON, pin, debounceMs) {
 
-  var longPressInProgress = false
+  private var pressedTime: Instant? = null
+  private var longPressTimer: Timer? = null
+  private var longPressDone = false
 
   override fun initDevice() {
     super.initDevice()
@@ -29,18 +33,23 @@ class SwitchButtonDevice(
   override fun additionalOnStateChanged(newState: String) {
     super.additionalOnStateChanged(newState)
     if (newState == PRESSED_STATE_VALUE) {
-      thread {
-        Thread.sleep(longPressTimeMs)
-        if (pin.isLow) {
+      pressedTime = Instant.now()
+      longPressTimer = Timer("SwitchButtonLongPress", false)
+      longPressTimer!!.schedule(longPressTimeMs) {
+        if (pressedTime?.isBefore(Instant.now().minusMillis(longPressTimeMs)) == true) {
           LOGGER.debug { "SwitchButtonDevice '$id' pressed for more then $longPressTimeMs ms" }
-          longPressInProgress = true
           notify(updatableProperty(), LONG_PRESSED_STATE_VALUE)
+          longPressDone = true
         }
       }
-    } else if (longPressInProgress) {
+    } else if (longPressDone && newState == RELEASED_STATE_VALUE) {
       LOGGER.debug { "SwitchButtonDevice '$id' released after long press" }
-      longPressInProgress = false
+      longPressDone = false
+      pressedTime = null
       notify(updatableProperty(), LONG_RELEASED_STATE_VALUE)
+    } else {
+      longPressTimer?.cancel()
+      pressedTime = null
     }
   }
 
