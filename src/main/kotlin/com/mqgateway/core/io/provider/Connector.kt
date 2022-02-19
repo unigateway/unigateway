e1fb29627a652a4a512275da8523c74fd06c94fb
Mariusz Józala <mariusz@aetas.pl>
2022-02-19 12:19:32 +0100

    Going back to Kotlin 1.4 for now to make tests pass again Need to change sealed interfaces to sealed classes because of that

--- src/main/kotlin/com/mqgateway/core/io/provider/Connector.kt
+++ src/main/kotlin/com/mqgateway/core/io/provider/Connector.kt
@@ -3,13 +3,13 @@ package com.mqgateway.core.io.provider
 /**
  * Configuration used by InputOutputProviders to instantiate input/output interface (i.e. BinaryInput, BinaryOutput etc.)
  */
-sealed interface Connector
+sealed class Connector
 
 /**
  * Configuration required by the hardware interface to instantiate concrete hardware input/output interface (i.e. BinaryInput, BinaryOutput etc.)
  */
-interface HardwareConnector : Connector
+abstract class HardwareConnector : Connector()
 
 data class MySensorsConnector(
   val nodeId: Int
-) : Connector
+) : Connector()
