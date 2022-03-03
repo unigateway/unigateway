74e21fabcb4ded6c5ca0b6414470361c67723fce
Michał Klekowski <kleko09@gmail.com>
2022-03-03 13:10:38 +0100

    #14 Convert BinaryStateConverter class to object

--- src/main/kotlin/com/mqgateway/core/hardware/raspberrypi/BinaryStateConverter.kt
+++ src/main/kotlin/com/mqgateway/core/hardware/raspberrypi/BinaryStateConverter.kt
@@ -2,14 +2,12 @@ package com.mqgateway.core.hardware.raspberrypi
 
 import com.mqgateway.core.io.BinaryState
 
-class BinaryStateConverter {
-  companion object {
-    fun fromBoolean(value: Boolean): BinaryState {
-      return if (value) BinaryState.HIGH else BinaryState.LOW
-    }
+object BinaryStateConverter {
+  fun fromBoolean(value: Boolean): BinaryState {
+    return if (value) BinaryState.HIGH else BinaryState.LOW
+  }
 
-    fun toBoolean(binaryState: BinaryState): Boolean {
-      return binaryState == BinaryState.HIGH
-    }
+  fun toBoolean(binaryState: BinaryState): Boolean {
+    return binaryState == BinaryState.HIGH
   }
 }
