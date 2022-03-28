9b4b34ba987f76fc37a5d421cf17c9af64e87a35
Michał Klekowski <kleko09@gmail.com>
2022-03-28 21:21:55 +0200

    #15 Remove unnecessary comments

--- src/main/kotlin/com/mqgateway/core/device/DeviceRegistry.kt
+++ src/main/kotlin/com/mqgateway/core/device/DeviceRegistry.kt
@@ -2,15 +2,12 @@ package com.mqgateway.core.device
 
 import com.mqgateway.core.device.unigateway.UniGatewayDevice
 
-// todo if we use it e.g. in HomeiFactory to get devices,
-//  should it return something different than Device to not have possibility to call .init or addListener?
 class DeviceRegistry(val devices: Set<Device>) {
 
   fun getById(deviceId: String): Device? = devices.find { it.id == deviceId }
 
   fun filterByType(type: DeviceType): List<Device> = devices.filter { it.type == type }
 
-  // todo how it will be when we have a cluster? ;)
   fun getUniGatewayDevice(): UniGatewayDevice {
     val unigatewayDevices: List<Device> = filterByType(DeviceType.UNIGATEWAY)
     if (unigatewayDevices.size != 1) {
