5997be7d20303ab56432076b9d492599ff7cac8b
Mariusz Józala <qmy@kmd.dk>
2022-08-10 23:47:46 +0200

    Bash script to prepare SD card for MqGateway #59

---
+++ installation-scripts/mqgateway/start_unigateway.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /opt/unigateway || exit
+
+export GATEWAY_SYSTEM_PLATFORM=MQGATEWAY
+export GATEWAY_SYSTEM_MYSENSORS_ENABLED=true
+export GATEWAY_SYSTEM_MYSENSORS_PORT_DESCRIPTOR=/myserial
+export GATEWAY_MQTT_ENABLED=false
+# export GATEWAY_MQTT_HOSTNAME=192.168.1.100
+
+/opt/java/bin/java -Xms64m -Xmx64m -jar unigateway.jar
