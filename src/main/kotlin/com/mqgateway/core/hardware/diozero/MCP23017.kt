773c6492d480c5cb3c4fdec458dfdca8d849f209
Mariusz Józala <mariusz@aetas.pl>
2023-01-26 23:01:04 +0100

    Modify diozero implementation of MCP23017 to avoid changing states of MCP23017 pins on MqGateway hardware reboot PR mattjlewis/diozero#126 created with the same change

--- src/main/kotlin/com/mqgateway/core/hardware/diozero/MCP23017.kt
+++ src/main/kotlin/com/mqgateway/core/hardware/diozero/MCP23017.kt
@@ -1,7 +1,10 @@
 package com.mqgateway.core.hardware.diozero
 
+import com.diozero.api.GpioEventTrigger
+import com.diozero.api.GpioPullUpDown
 import com.diozero.api.I2CDevice
 import com.diozero.devices.mcp23xxx.MCP23x17
+import com.diozero.util.BitManipulation
 import com.diozero.util.MutableByte
 import org.tinylog.Logger
 
@@ -10,10 +13,12 @@ import org.tinylog.Logger
  * It is modifying how MCP is initialised to avoid zeroing of all pins, which may cause light blinking or gates opening on UniGateway restart.
  * This class also do not support interrupts for now, but it could be potentially implemented.
  */
-open class MCP23017(controller: Int, address: Int, interruptGpioA: Int, interruptGpioB: Int) :
+open class MCP23017(private val controller: Int, private val address: Int, interruptGpioA: Int, interruptGpioB: Int) :
   MCP23x17("$DEVICE_NAME-$controller-$address", interruptGpioA, interruptGpioB) {
 
   private val device = I2CDevice.builder(address).setController(controller).build()
+  private val directions = arrayOf(MutableByte(), MutableByte())
+  private val pullUps = arrayOf(MutableByte(), MutableByte())
 
   init {
     initialiseWithoutZeroing()
@@ -45,8 +50,11 @@ open class MCP23017(controller: Int, address: Int, interruptGpioA: Int, interrup
       writeByte(getIOConReg(0), iocon.value)
     }
     for (port in 0 until NUM_PORTS) {
-      // Default all GPIOs to output
-      writeByte(getIODirReg(port), 0.toByte())
+      // Read currently set directions
+      directions[port] = MutableByte(readByte(getIODirReg(port)))
+      // Read current state of pull-up resistors
+      pullUps[port] = MutableByte(readByte(getGPPullUpReg(port)))
+
       // Default to normal input polarity - IPOLA/IPOLB
       writeByte(getIPolReg(port), 0.toByte())
       // Disable interrupt-on-change for all GPIOs
@@ -55,10 +63,57 @@ open class MCP23017(controller: Int, address: Int, interruptGpioA: Int, interrup
       writeByte(getDefValReg(port), 0.toByte())
       // Disable interrupt comparison control
       writeByte(getIntConReg(port), 0.toByte())
-      // Disable pull-up resistors
-      writeByte(getGPPullUpReg(port), 0.toByte())
     }
   }
+
+  override fun setInputMode(gpio: Int, pud: GpioPullUpDown, trigger: GpioEventTrigger) {
+    val bit = (gpio % GPIOS_PER_PORT).toByte()
+    val port = gpio / GPIOS_PER_PORT
+
+    // Set the following values: direction, pullUp, interruptCompare, defaultValue,
+    // interruptOnChange
+    directions[port].setBit(bit)
+    writeByte(getIODirReg(port), directions[port].value)
+    val new_dir = readByte(getIODirReg(port))
+    if (directions[port].value != new_dir) {
+      Logger.error(
+        "Error setting input mode for gpio {}, expected {}, read {}", Integer.valueOf(gpio),
+        java.lang.Byte.valueOf(directions[port].value), java.lang.Byte.valueOf(new_dir)
+      )
+    }
+    if (pud == GpioPullUpDown.PULL_UP) {
+      pullUps[port].setBit(bit)
+      writeByte(getGPPullUpReg(port), pullUps[port].value)
+    }
+  }
+
+  override fun setOutputMode(gpio: Int) {
+    val bit = (gpio % GPIOS_PER_PORT).toByte()
+    val port = gpio / GPIOS_PER_PORT
+
+    // Set the following values: direction, pullUp, interruptCompare, defaultValue,
+    // interruptOnChange
+    directions[port].unsetBit(bit)
+    writeByte(getIODirReg(port), directions[port].value)
+  }
+
+  override fun setValue(gpio: Int, value: Boolean) {
+    require(!(gpio < 0 || gpio >= NUM_GPIOS)) {
+      ("Invalid GPIO: " + gpio + ". " + deviceName() + " has " + NUM_GPIOS + " GPIOs; must be 0.." + (NUM_GPIOS - 1))
+    }
+    val bit = (gpio % GPIOS_PER_PORT).toByte()
+    val port = gpio / GPIOS_PER_PORT
+
+    // Check the direction of the GPIO - can't set the output value for input GPIOs
+    // (direction bit is set)
+    check(!directions[port].isBitSet(bit.toInt())) { "Can't set value for input GPIO: $gpio" }
+    // Read the current state of this bank of GPIOs
+    val oldVal = readByte(getGPIOReg(port))
+    val newVal = BitManipulation.setBitValue(oldVal, bit.toInt(), value)
+    writeByte(getOLatReg(port), newVal)
+  }
+
+  private fun deviceName() = "$DEVICE_NAME-$controller-$address"
 
   companion object {
     private const val DEVICE_NAME = "MCP23017"
@@ -68,5 +123,6 @@ open class MCP23017(controller: Int, address: Int, interruptGpioA: Int, interrup
     private const val IOCON_HAEN_BIT: Byte = 3
     private const val IOCON_ODR_BIT: Byte = 2
     private const val NUM_PORTS = 2
+    private const val NUM_GPIOS = NUM_PORTS * 8
   }
 }
